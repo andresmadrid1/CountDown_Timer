dcce05757f22a82c37c36c03061b018c120dc44b
andresnaranjo <andresmnm@hotmail.com>
2022-09-05 19:26:07 -0500

    Countdown timer v1.1

---
+++ cutdown_Timer.py
@@ -0,0 +1,13 @@
+
+#This program allows to have the count timer of multiple selections:
+# 1. The user can select countdown timer for minutes, seconds.
+# 2. The user can get the number of days beetwen a specific day and current day.
+# 3. The user can get the number of days between 2 specific days.
+# 4. The user can get the number of year between a specific dat and current year.
+
+
+def run():
+    pass
+
+if __name__=='__main__':
+    run()
